6ed82519a3463435a3fa4ddf1d0d8adcd4732b5c
Juraj <juraj.begovac@bellabeat.com>
2016-09-07 22:22:17 +0200

    Initial commit

---
+++ app/src/androidTest/java/hr/beg/mvplibsexploring/ApplicationTest.java
@@ -0,0 +1,13 @@
+package hr.beg.mvplibsexploring;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
