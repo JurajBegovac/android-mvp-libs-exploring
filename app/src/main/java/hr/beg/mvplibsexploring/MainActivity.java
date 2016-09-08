451e83a5063000897c7674ee137f1d41600f57e2
Juraj <juraj.begovac@bellabeat.com>
2016-09-08 09:32:18 +0200

    Add flow dependency

--- app/src/main/java/hr/beg/mvplibsexploring/MainActivity.java
+++ app/src/main/java/hr/beg/mvplibsexploring/MainActivity.java
@@ -1,12 +1,28 @@
 package hr.beg.mvplibsexploring;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import flow.Flow;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    protected void attachBaseContext(Context baseContext) {
+        baseContext = Flow.configure(baseContext, this).install();
+        super.attachBaseContext(baseContext);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (!Flow.get(this).goBack()) {
+            super.onBackPressed();
+        }
     }
 }
